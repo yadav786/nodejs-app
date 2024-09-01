f8920204e1a3d7c6d90e504ed1b44151fd19a732
Pankaj Yadav <Pankaj.Yadav@mastercard.com>
2024-09-01 14:20:52 +0530

    deploy to ec2

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://admin:${password}@cluster0.cjd5b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(connectionString);
 let conn;
 try {
